675722193a0aab579d297e98ea4b8ef75b440289
Suthakar Ganeshamoorthy <suthakar@navotar.com>
2021-08-16 19:46:24 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+import {Home} from './components/Home';
+import {Department} from './components/Department';
+import {Employee} from './components/Employee';
+import {Navigation} from './components/Navigation';
+
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>    
+    <div className="container">
+      <h3 className="m-3 d-flex justify-content-center">
+        React JS Tutorial
+      </h3>
+
+      <Navigation />
+
+      <Switch>
+          <Route path='/' component={Home} exact />
+          <Route path='/department' component={Department} />
+          <Route path='/employee' component={Employee} />
+
+      </Switch>
+
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
