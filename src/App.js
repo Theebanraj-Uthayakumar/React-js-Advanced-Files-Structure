062364d237a13eeb9b75a6153999686114a72b1a
Suthakar Ganeshamoorthy <suthakar@navotar.com>
2021-08-20 16:57:28 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <BrowserRouter>    
     <div className="container">
       <h3 className="m-3 d-flex justify-content-center">
-        React JS Tutorial
+        React JS Tutorials
       </h3>
 
       <Navigation />
